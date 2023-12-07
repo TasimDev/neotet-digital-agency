11cd202814301019a1c138286ce5cc831f61ec31
Tasim <118922118+TasimDev@users.noreply.github.com>
2023-12-07 20:20:38 +0200

    dsfgfdsg

--- app/page.js
+++ app/page.js
@@ -8,19 +8,13 @@ export default function Home() {
 
       <div id="bg_stars"></div>
       <div className="home__title">
-
+        {/* 
         <h1>Reach <span className="thin">perfection</span> and create culture <br /> with <span className="thin">new
-          generation</span> web products <br /> for determine the <span className="thin">future</span>. </h1>
+          generation</span> web products <br /> for determine the <span className="thin">future</span>. </h1> */}
 
-        <span className="text-gray">New Generation Web Agency. </span><br /><br />
-        <span className="stars">
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-        </span><br />
-        <span className="text-gray">36 reviews</span>
+        <span className="text-gray">New Generation Web Agency. </span><br /><br /><br /><br />
+      
+
       </div>
     </main>
   )
--- app/ui/Navbar/nav-link-mobile.jsx
+++ app/ui/Navbar/nav-link-mobile.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const links = [
+    { name: 'Home', href: '/', title: 'Neotet | Digital Agency' },
     { name: 'Services', href: '/services', title: 'Learn more about Services to our comoany, Neotet - Digital Agency.' },
     { name: 'Projects', href: '/projects', title: 'Projects created by Neotet - Digital Agency' },
     { name: 'Process', href: '/process', title: 'Learn more about the process, and how we work with our clients of out company, Neotet - Digital Agency' },
